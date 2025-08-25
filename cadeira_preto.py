0f2c1729a04ee6c4a10b09a1dba0dc6b0edc4c85
joao.fidelis <joao.fidelis@maxiconsystems.com.br>
2025-08-25 20:38:57 -0300

    feat: Questão 1

---
+++ cadeira_preto.py
@@ -0,0 +1,24 @@
+'''
+João pedro fidelis - grupo 6
+
+1.
+a) oque é um cabeçario dentro de um arquivo python? para que serve?
+    Um cabeçalho dentro de um arquivo python é uma serie de comentarios nas
+    primeiras linhas do arquivo que dizem algumas informações importantes
+    de serem lembradas, normalmente é separado em 5 partes tendo primeiramente
+    um código de execução do script via cmd que descreve o formato especifico
+    do script (python3, python, py); a segunda parte tendo o tipo de codificação
+    deste arquivo (utf8 por exemplo); a terceira parte dizendo quem criou esse arquivo,
+    que dia e qual versão dele; a terceira parte tendo uma breve descrição do projeto;
+    e por fim como ultima parte se tem os imports padrão do python.
+
+b) qual a diferença do cabeçario para a docstring? oque é uma docstring?
+    Uma docstring é um comentario dentro de uma função que tem como objetivo especificar
+    internamente como essa função funciona, sempre é o primeiro comentario de uma ou
+    varias linhas dentro de uma função, e pode ser acessado utilizando a notação __doc__.
+
+    A diferença entre um cabeçario e docstring é que um cabeçario fica no inicio do arquivo
+    python e serve apenas para especificar informações estritamente do arquivo, enquanto o
+    docstring é uma documentação feita sempre em inicio de funções para especificar como
+    elas funcional internamente, além disso o desenvolvedor tem acesso a elas.
+'''
